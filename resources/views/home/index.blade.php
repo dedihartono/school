dc29eeaae70a379a267e17dcdfcd00d6abee76cb
Dedi Hartono <dedi.hartono@mad.co.id>
2023-12-29 21:15:08 +0800

    Update templating

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -2,6 +2,34 @@
 
 @section('title', 'School')
 
+@section('sliders')
+    @include('home.components.hero')
+@endsection
+
 @section('content')
+
+    <!-- ======= About Section ======= -->
+    @include('home.components.about')
+    <!-- End About Section -->
+
+    <!-- ======= Counts Section ======= -->
+    @include('home.components.counts')
+    <!-- End Counts Section -->
+
+    <!-- ======= Why Us Section ======= -->
+    @include('home.components.why-us')
+    <!-- End Why Us Section -->
+
+    <!-- ======= Features Section ======= -->
+    @include('home.components.features')
+    <!-- End Features Section -->
+
+    <!-- ======= Popular Courses Section ======= -->
+    @include('home.components.course')
+    <!-- End Popular Courses Section -->
+
+    <!-- ======= Teachers Section ======= -->
+    @include('home.components.teachers')
+    <!-- End Teachers Section -->
 
 @endsection
