79f46eda67bdaeaa87966bd8ddf2000febad7eba
Dedi Hartono <dedi.hartono@mad.co.id>
2024-01-01 15:01:34 +0800

    video

--- resources/views/home/components/hero.blade.php
+++ resources/views/home/components/hero.blade.php
@@ -1,7 +1,15 @@
 <section id="hero" class="d-flex justify-content-center align-items-center">
-    <div class="container position-relative" data-aos="zoom-in" data-aos-delay="100">
-        <h1>Learning Today,<br>Leading Tomorrow</h1>
-        <h2>We are team of talented designers making websites with Bootstrap</h2>
-        <a href="#" class="btn-get-started">Info PPDB</a>
+    <div class="item">
+        <div class="container-fluid container-custom">
+            <video id="video" autoplay muted loop>
+                <source src="{{ asset('assets/templates/Mentor/assets/img/video-1.mp4') }}" type="video/mp4">
+                Your browser does not support the video tag.
+            </video>
+            <div class="text-overlay mx-auto" data-aos="zoom-in" data-aos-delay="100">
+                <h1>Learning Today,<br>Leading Tomorrow</h1>
+                <h2>We are team of talented designers making websites with Bootstrap</h2>
+                <a href="#" class="btn-get-started">Info PPDB</a>
+            </div>
+        </div>
     </div>
 </section>
